14bf3fb37b4c2be82482fb7860b6c3f980f0cf68
Erick Romero <99360659+ErickRomeroZ@users.noreply.github.com>
2022-05-15 13:06:33 -0500

    cambios del footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,18 +2,17 @@
 
     <footer>
 
-        <div class="contacto">
-        <h4>Text</h4>
-        <p>AAAAAAAAAAAAA</p>
-        <p>AAAAAAAAAAAA</p>
-        <p> AAAAAAAAAAA -</p>      
+        <div class="logo">
+        <img src="../assets/logo3.png" alt="Logo" width="100" height="100">
+            
         </div>
 
-        <div class="privacidad">
+        <div class="Otrasorg">
             <ul>
-                <li><a href="#">Link1</a></li>
-                <li><a href="#">Link2.</a></li>
-                <li><a href="#">Link3.</a></li>
+                <p>Otras organizaciones</p>
+                <li><a href="https://mx.oceana.org/">OCEANA</a></li>
+                <li><a href="https://www.greenpeace.org/mexico/">GREENPACE</a></li>
+                <li><a href="https://vitalis.net/">VITALIS</a></li>
             </ul>           
         </div>
         
